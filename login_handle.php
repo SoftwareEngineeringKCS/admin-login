3673c972d5f03c92cfcd86bec5ce804860150a42
Jorge Espada <32748368+espadajo@users.noreply.github.com>
2017-12-13 20:13:48 -0500

    Update login_handle.php

--- login_handle.php
+++ login_handle.php
@@ -1,51 +1,58 @@
 <?php 
 
-	$page_title = 'Kean Career Services';
-	include ('includes/header.html');
-	include ('includes/db_config.php');
+	if (!isset($_POST['login_username'])) {
+		echo "<h1>Forbidden</h1>";
+		echo "<p style='font-size: 1.2em'>You don't have permission to access this page.</p>";
+	} else {
 
-	// Add the POST data to initial cookies.
-	error_reporting(E_ALL | E_STRICT);
+		$page_title = 'Kean Career Services';
+		include ('includes/header.html');
+		include ('includes/db_config.php');
+		// Add the POST data to initial cookies.
+		error_reporting(E_ALL | E_STRICT);
 
-	// Verify user.
-	$query = sprintf("SELECT * FROM Consultants WHERE UPPER(username) = '%s'", strtoupper($_POST['login_username']));
-	$result = mysqli_query($conex, $query);
+		// Verify user.
+		$query = sprintf("SELECT * FROM Consultants WHERE UPPER(username) = '%s'", strtoupper($_POST['login_username']));
+		$result = mysqli_query($conex, $query);
 
-	if ($result) {
-		if (mysqli_num_rows($result) > 0) {
-			// Verify username and password.
-			$query = sprintf("SELECT id, username, password FROM Consultants WHERE UPPER(username) = '%s' AND password = '%s'", strtoupper($_POST['login_username']), $_POST['login_password']);
-			$result = mysqli_query($conex, $query);
+		if ($result) {
+			if (mysqli_num_rows($result) > 0) {
+				// Verify username and password.
+				$query = sprintf("SELECT id, first_name, last_name, username, password FROM Consultants WHERE UPPER(username) = '%s' AND password = '%s'", strtoupper($_POST['login_username']), $_POST['login_password']);
+				$result = mysqli_query($conex, $query);
 
-			if ($result) {
-				if (mysqli_num_rows($result) > 0) {
-					$row = mysqli_fetch_array($result);
-					session_start();
-					$_SESSION["user_id"] = $row[0];
-					setcookie("logged_in", TRUE, time() + 86400); // Logged in
-					header("Location: staff.php");
+				if ($result) {
+					if (mysqli_num_rows($result) > 0) {
+						$row = mysqli_fetch_array($result);
+						session_start();
+						$_SESSION["user_id"] = $row[0];
+						$_SESSION["user_fname"] = $row[1];
+						setcookie("logged_in", TRUE, time() + 86400); // Logged in
+						header("Location: staff.php");
+					} else {
+						echo "<p class='error'>User \"" . $_POST['login_username'] . "\" exists but password does not match.</p>";
+						echo "<br><a href='staff.php'>TRY AGAIN</a>";
+					}
 				} else {
-					echo "<p class='error'>User \"" . $_POST['login_username'] . "\" exists but password does not match.</p>";
+					echo "<p class='error'>Problem trying to login: " . mysqli_error();
+					echo "<br>Contact Administrator!</p>";
 					echo "<br><a href='staff.php'>TRY AGAIN</a>";
 				}
 			} else {
-				echo "<p class='error'>Problem trying to login: " . mysqli_error();
-				echo "<br>Contact Administrator!</p>";
+				echo "<p class='error'>User \"" . $_POST['login_username'] . "\" does not exist in the database.</p>";
 				echo "<br><a href='staff.php'>TRY AGAIN</a>";
 			}
 		} else {
-			echo "<p class='error'>User \"" . $_POST['login_username'] . "\" does not exist in the database.</p>";
+			echo "<p class='error'>Problem trying to login: " . mysqli_error();
+			echo "<br>Contact Administrator!</p>";
 			echo "<br><a href='staff.php'>TRY AGAIN</a>";
 		}
-	} else {
-		echo "<p class='error'>Problem trying to login: " . mysqli_error();
-		echo "<br>Contact Administrator!</p>";
-		echo "<br><a href='staff.php'>TRY AGAIN</a>";
+
+		// Free resources.
+		mysqli_free_result($result);
+		mysqli_close($conex);
+
+		include ('includes/footer.html');
 	}
 
-	// Free resources.
-	mysqli_free_result($result);
-	mysqli_close($conex);
-
-	include ('includes/footer.html');
 ?>
